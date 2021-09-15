86785ab515049e2ecaa519edb2f198fe05eba54b
Darián Capote Quintana <dariancapoteq@gmail.com>
2021-09-14 23:01:09 -0400

    Add connection to MongoDB and middleware

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
-const router_cars = require('./routes/cars');
-
+const mongoose = require('mongoose');
+const router_user = require('./routes/user');
 const app = express();
 
-app.use('/cars', router_cars);
+// Middleware 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Include routes
+app.use(router_user);
+
+// Conect to mongoDB
+const mongoDB_url = 'mongodb://localhost:27017/admin';
+mongoose.connect(mongoDB_url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+var db = mongoose.connection;
+db.on('open', () => console.log('Conection success'));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/', (req, res) => {
   res.send('My Express Example!');
